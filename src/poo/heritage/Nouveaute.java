4120d4c64de893d6a02a7252b6906f020aaf52ec
Admin <alexandre.kimtsaris@bstorm.be>
2021-06-30 14:27:50 +0200

    demo git

---
+++ src/poo/heritage/Nouveaute.java
@@ -0,0 +1,18 @@
+package poo.heritage;
+
+public class Nouveaute {
+
+    private String message;
+
+    public Nouveaute(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
