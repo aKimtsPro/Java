fb00e1023c41d321c0d0b65b9edd8268c2895e83
Admin <alexandre.kimtsaris@bstorm.be>
2021-07-23 09:08:22 +0200

    demo git

--- src/poo/generics/Bille.java
+++ src/poo/generics/Bille.java
@@ -3,7 +3,20 @@ package poo.generics;
 public class Bille implements Lancable {
 
     private int diametre;
+
+    public Bille(int diametre) {
+        this.diametre = diametre;
+    }
+
     public int lancer(){
-        return 15;
+        return 10 + diametre;
+    }
+
+    public int getDiametre() {
+        return diametre;
+    }
+
+    public void setDiametre(int diametre) {
+        this.diametre = diametre;
     }
 }
