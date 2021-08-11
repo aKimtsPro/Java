912233cef9dd563a99e596dce2c60b3c82be9e75
Admin <alexandre.kimtsaris@bstorm.be>
2021-08-11 16:01:48 +0200

    post writer

--- src/poo/io/ecriture/DemoWrite.java
+++ src/poo/io/ecriture/DemoWrite.java
@@ -11,47 +11,47 @@ public class DemoWrite {
 
         // FILEWRITER
 
-//        try ( FileWriter fw = new FileWriter("./ressources/writ.txt") ) {
-//
-//            fw.append("salut")
-//                    .append('c')
-//                    .append("omment")
-//                    .append("va?")
-//                    .append('\n');
-//
-//            fw.write("salut");
-//            fw.write('c');
-//            fw.write("omment");
-//            fw.write("va?");
-//
-//        }
-//        catch ( IOException ex ){
-//            System.out.println("probleme : " + ex.getMessage());
-//        }
+        try ( FileWriter fw = new FileWriter("./ressources/writ.txt", false) ) {
+
+            fw.append("salut")
+                    .append('c')
+                    .append("omment")
+                    .append("va?")
+                    .append('\n');
+
+            fw.write("salut");
+            fw.write('c');
+            fw.write("omment");
+            fw.write("va?");
+
+        }
+        catch ( IOException ex ){
+            System.out.println("probleme : " + ex.getMessage());
+        }
 
         //
 
-//        try (BufferedWriter bw = new BufferedWriter(new FileWriter("./ressources/write.txt", true))){
-//
-//            bw.append("salut ")
-//                    .append('c')
-//                    .append("omment ")
-//                    .append("va?");
-//
-//            bw.newLine();
-//
-//            bw.write("salut ");
-//            bw.write('c');
-//            bw.write("omment ");
-//            bw.write("va?");
-//
-//        }
-//        catch (IOException ex){
-//            System.out.println("probleme : " + ex.getMessage());
-//        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("./ressources/write.txt", true))){
+
+            bw.append("salut ")
+                    .append('c')
+                    .append("omment ")
+                    .append("va?");
+
+            bw.newLine();
+
+            bw.write("salut ");
+            bw.write('c');
+            bw.write("omment ");
+            bw.write("va?");
+
+        }
+        catch (IOException ex){
+            System.out.println("probleme : " + ex.getMessage());
+        }
 
 //          ajout au fichier possible:
-//      try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("./ressources/write.txt", true)))  ){
+//            try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("./ressources/write.txt", true)))  ){
 //          pas d'ajout au fichier possible:
         try( PrintWriter pw = new PrintWriter( "./ressources/wr.txt" )  ){
             System.out.println("fichier ouvert");
